ed91e44f296c907abbedaf9ba9413c82d902b80e
844817 <844817@SAIT225769IT.ACDM.DS.SAIT.CA>
2021-02-09 01:02:36 -0700

    services package added

---
+++ src/java/services/AccountService.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+/**
+ *
+ * @author 844817
+ */
+public class AccountService {
+    
+}
