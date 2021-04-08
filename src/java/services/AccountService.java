655aae3841640fccf0e84a0a33e4ace68725a281
Dan Quach <839225@SAIT225243IT.ACDM.DS.SAIT.CA>
2021-04-08 15:25:06 -0600

    Removed email sent for logins

--- src/java/services/AccountService.java
+++ src/java/services/AccountService.java
@@ -18,19 +18,13 @@ public class AccountService {
 
         try {
             Logins user = userDB.getUser(username);
-            final String TO = "oursafetyapplication@gmail.com";
-            final String SUBJECT = "Recent Login";
-            String body = user.getUsername() + " has just logged in.";
-            EmailService es = new EmailService();
-            
+
             if ((password.equals(user.getPassword()) && user.getUsername().equals("admin")) || (password.equals(user.getPassword()) && user.getUsername().equals("manager2")) || (password.equals(user.getPassword()) && user.getUsername().equals("oursafetyapplication@gmail.com"))) {
                 Logger.getLogger(AccountService.class.getName()).log(Level.INFO, "Successful login by {0}", username);
-                es.sendMail(TO, SUBJECT, body, false);
                 return user;
 
             } else if (ps.verifyPassword(password, user.getPassword()) == true) {
                 Logger.getLogger(AccountService.class.getName()).log(Level.INFO, "Successful login by {0}", username);
-                es.sendMail(TO, SUBJECT, body, false);
                 return user;
             }
         } catch (Exception e) {
